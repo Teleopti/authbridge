5580037ea1fb6d3751ae1ed6f9dfbd300aab278f
Robin Karlsson <robin.karlsson@teleopti.com>
2014-12-01 08:44:40 +0100

    Found some flaws in the relative open id client

--- src/AuthBridge/Clients/RelativeOpenIdClient.cs
+++ src/AuthBridge/Clients/RelativeOpenIdClient.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Reflection;
 using System.Web;
 using DotNetOpenAuth.OpenId;
+using DotNetOpenAuth.OpenId.Messages;
 using DotNetOpenAuth.OpenId.RelyingParty;
 using log4net;
 
@@ -29,20 +31,20 @@ namespace AuthBridge.Clients
 			var relyingParty = (OpenIdRelyingParty)relyingPartyField.GetValue(this);
 			var realm = new Realm(_realmUri ?? new Uri(returnUrl.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped)));
 			var userSuppliedIdentifier = new Uri((Identifier)providerIdentifierField.GetValue(this));
-			
-			Logger.InfoFormat("Request {0}; userSuppliedIdentifier {1}", request, userSuppliedIdentifier);
-			IAuthenticationRequest authenticationRequest = relyingParty.CreateRequest(userSuppliedIdentifier, realm, returnUrl);
+			var localhost = new Uri("http://localhost");
+			var userSuppliedIdentifierForRequestMachine = new Uri(localhost,
+				new Uri(userSuppliedIdentifier.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped)).MakeRelativeUri(userSuppliedIdentifier));
+
+			Logger.InfoFormat("Request {0}; userSuppliedIdentifier {1}; userSuppliedIdentifierForRequestMachine {2}", request, userSuppliedIdentifier, userSuppliedIdentifierForRequestMachine);
+			IAuthenticationRequest authenticationRequest = relyingParty.CreateRequest(userSuppliedIdentifierForRequestMachine, realm, returnUrl);
 			OnBeforeSendingAuthenticationRequest(authenticationRequest);
 
 			try
 			{
-				var property = authenticationRequest.DiscoveryResult.GetType().GetProperty("UserSuppliedIdentifier");
-				var site = new Uri(context.Request.Url.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped));
-				var userSupplied = new Uri(site,
-					new Uri(userSuppliedIdentifier.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped)).MakeRelativeUri(userSuppliedIdentifier));
+				var property = authenticationRequest.DiscoveryResult.GetType().GetProperty("ProviderEndpoint");
 				
-				Logger.InfoFormat("Request {0}; userSupplied {1}", request, userSupplied);
-				property.SetValue(authenticationRequest.DiscoveryResult, Identifier.Parse(userSupplied.ToString()), BindingFlags.SetProperty, null, null, CultureInfo.CurrentCulture);
+				Logger.InfoFormat("Request {0}; userSupplied {1}", request, userSuppliedIdentifier);
+				property.SetValue(authenticationRequest.DiscoveryResult, userSuppliedIdentifier, BindingFlags.SetProperty, null, null, CultureInfo.CurrentCulture);
 				
 				authenticationRequest.RedirectToProvider();
 			}
