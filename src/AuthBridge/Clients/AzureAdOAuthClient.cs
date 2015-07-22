d29ed94ffa8a791b4c619666e6e3ae275918d199
kunning <lilliput8551@gmail.com>
2015-07-22 15:41:16 +0200

    add azure ad oauth authentication

---
+++ src/AuthBridge/Clients/AzureAdOAuthClient.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using AuthBridge.Clients.Util;
+using DotNetOpenAuth.AspNet.Clients;
+
+namespace AuthBridge.Clients
+{
+	public class AzureAdOAuthClient : OAuth2Client
+	{
+		private readonly string _authorizationEndpoint;
+		private readonly string _tokenEndpoint;
+		private readonly string _graphApiEndpoint;
+		private readonly string _graphApiVersion;
+
+		private const string GraphApiResource = "https://graph.windows.net";
+
+		private readonly string _appId;
+		private readonly string _appSecret;
+
+		public AzureAdOAuthClient(string appId, string secretKey, string graphApiEndpoint, string tokenEndpoint, string authorizationEndpoint, string graphApiVersion)
+			: this("AzureAdOAuth", appId, secretKey, graphApiEndpoint, tokenEndpoint, authorizationEndpoint, graphApiVersion)
+		{
+		}
+
+		protected AzureAdOAuthClient(string providerName, string appId, string appSecret, string graphApiEndpoint, string tokenEndpoint, string authorizationEndpoint, string graphApiVersion)
+			: base(providerName)
+		{
+			_appId = appId;
+			_appSecret = appSecret;
+			_graphApiEndpoint = graphApiEndpoint;
+			_tokenEndpoint = tokenEndpoint;
+			_authorizationEndpoint = authorizationEndpoint;
+			_graphApiVersion = graphApiVersion;
+		}
+
+		protected override Uri GetServiceLoginUrl(Uri returnUrl)
+		{
+			var builder = new UriBuilder(_authorizationEndpoint);
+			builder.AppendQueryArgs(
+				new Dictionary<string, string> {
+					{ "client_id", _appId },
+					{ "response_type", "code" },
+					{ "redirect_uri", returnUrl.AbsoluteUri },
+				});
+
+			return builder.Uri;
+		}
+
+		protected override IDictionary<string, string> GetUserData(string accessToken)
+		{
+			AzureAdUserData graph;
+			var request = WebRequest.Create(string.Format("{0}/me?api-version={1}", _graphApiEndpoint, _graphApiVersion));
+			request.Headers.Add(HttpRequestHeader.Authorization.ToString(), string.Format("Bearer {0}", accessToken));
+			using (var response = request.GetResponse())
+			{
+				using (var responseStream = response.GetResponseStream())
+				{
+					graph = JsonHelper.Deserialize<AzureAdUserData>(responseStream);
+				}
+			}
+
+			var userData = new Dictionary<string, string>();
+			userData.AddItemIfNotEmpty("id", graph.userPrincipalName);
+			userData.AddItemIfNotEmpty("objectId", graph.objectId);
+			userData.AddItemIfNotEmpty("username", graph.userPrincipalName);
+			userData.AddItemIfNotEmpty("name", graph.displayName);
+			return userData;
+		}
+
+		protected override string QueryAccessToken(Uri returnUrl, string authorizationCode)
+		{
+			var entity =
+				MessagingUtilities.CreateQueryString(
+					new Dictionary<string, string> {
+						{ "client_id", _appId },
+						{ "redirect_uri", returnUrl.AbsoluteUri },
+						{ "client_secret", _appSecret },
+						{ "code", authorizationCode },
+						{ "grant_type", "authorization_code" },
+						{"resource",GraphApiResource}
+					});
+
+			var tokenRequest = WebRequest.Create(_tokenEndpoint);
+			tokenRequest.ContentType = "application/x-www-form-urlencoded";
+			tokenRequest.ContentLength = entity.Length;
+			tokenRequest.Method = "POST";
+
+			using (var requestStream = tokenRequest.GetRequestStream())
+			{
+				var writer = new StreamWriter(requestStream);
+				writer.Write(entity);
+				writer.Flush();
+			}
+
+			var tokenResponse = (HttpWebResponse)tokenRequest.GetResponse();
+			if (tokenResponse.StatusCode == HttpStatusCode.OK)
+			{
+				using (var responseStream = tokenResponse.GetResponseStream())
+				{
+					var tokenData = JsonHelper.Deserialize<OAuth2AccessTokenData>(responseStream);
+					if (tokenData != null)
+					{
+						return tokenData.AccessToken;
+					}
+				}
+			}
+
+			return null;
+		}
+	}
+
+	public class AzureAdUserData
+	{
+		public string objectId;
+		public string userPrincipalName;
+		public string displayName;
+	}
+}
---
+++ src/AuthBridge/Protocols/OAuth/AzureAdOAuthHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using AuthBridge.Clients;
+using Microsoft.IdentityModel.Claims;
+using AuthBridge.Model;
+using System.Net;
+using System.IO;
+using DotNetOpenAuth.AspNet;
+
+namespace AuthBridge.Protocols.OAuth
+{
+	public class AzureAdOAuthHandler : ProtocolHandlerBase
+    {
+        private readonly ClaimProvider issuer;
+        private readonly string _appId;
+        private readonly string _secretKey;
+		private readonly string _graphApiEndpoint;
+		private readonly string _tokenEndpoint;
+		private readonly string _authorizationEndpoint;
+		private readonly string _graphApiVersion;
+
+		public AzureAdOAuthHandler(ClaimProvider issuer) : base(issuer)
+        {
+            if (issuer == null)
+                throw new ArgumentNullException("issuer");
+
+            this.issuer = issuer;
+			_appId = this.issuer.Parameters["clientId"];
+			_secretKey = this.issuer.Parameters["key"];
+			_graphApiEndpoint = this.issuer.Parameters["graphApiEndpoint"];
+			_tokenEndpoint = this.issuer.Parameters["tokenEndpoint"];
+			_authorizationEndpoint = this.issuer.Parameters["authorizationEndpoint"];
+			_graphApiVersion = this.issuer.Parameters["graphApiVersion"];
+        }
+
+        public override void ProcessSignInRequest(Scope scope, HttpContextBase httpContext)
+        {
+			var client = new AzureAdOAuthClient(_appId, _secretKey, _graphApiEndpoint, _tokenEndpoint, _authorizationEndpoint, _graphApiVersion);
+            client.RequestAuthentication(httpContext, MultiProtocolIssuer.ReplyUrl);
+        }
+
+        public override IClaimsIdentity ProcessSignInResponse(string realm, string originalUrl, HttpContextBase httpContext)
+        {
+			var client = new AzureAdOAuthClient(_appId, _secretKey, _graphApiEndpoint, _tokenEndpoint, _authorizationEndpoint, _graphApiVersion);
+            AuthenticationResult result;
+            try
+            {
+                result = client.VerifyAuthentication(httpContext, this.MultiProtocolIssuer.ReplyUrl);
+            }
+            catch (WebException wex)
+            {
+                throw new InvalidOperationException(new StreamReader(wex.Response.GetResponseStream()).ReadToEnd(), wex);
+            }
+
+            var claims = new List<Claim>
+                {
+                    new Claim(System.IdentityModel.Claims.ClaimTypes.NameIdentifier, result.ExtraData["id"])
+                };
+
+			return new ClaimsIdentity(claims, "AzureAd");
+        }      
+    }
+}
