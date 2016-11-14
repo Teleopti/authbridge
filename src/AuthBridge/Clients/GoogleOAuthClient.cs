527f907f3dc725f5669c1532cc13df1c37388e04
kunning <lilliput8551@gmail.com>
2016-11-14 14:02:19 +0100

    added google using oauth

---
+++ src/AuthBridge/Clients/GoogleOAuthClient.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using AuthBridge.Clients.Util;
+using DotNetOpenAuth.AspNet.Clients;
+
+namespace AuthBridge.Clients
+{
+	public class GoogleOAuthClient : OAuth2Client
+	{
+		private readonly string _clientId;
+		private readonly string _clientSecret;
+		private string _authorizationEndpoint= "https://accounts.google.com/o/oauth2/auth";
+		private string _tokenEndpoint = "https://accounts.google.com/o/oauth2/token";
+		private string _userInfoEndpoint = "https://www.googleapis.com/oauth2/v1/userinfo";
+
+		public GoogleOAuthClient(string clientId, string clientSecret):this("GoogleOAuthClient", clientId, clientSecret)
+		{
+		}
+
+		public GoogleOAuthClient(string providerName, string clientId, string clientSecret) : base(providerName)
+		{
+			this._clientId = clientId;
+			this._clientSecret = clientSecret;
+		}
+
+		protected override Uri GetServiceLoginUrl(Uri returnUrl)
+		{
+			var builder = new UriBuilder(_authorizationEndpoint);
+			builder.AppendQueryArgs(
+				new Dictionary<string, string>
+				{
+					{"response_type", "code"},
+					{"redirect_uri", returnUrl.AbsoluteUri},
+					{"scope", "https://www.googleapis.com/auth/userinfo.email"},
+					{"client_id", _clientId}
+				});
+
+			return builder.Uri;
+		}
+
+		protected override IDictionary<string, string> GetUserData(string accessToken)
+		{
+			GoogleUserData graph;
+			var request = WebRequest.Create($"{_userInfoEndpoint}");
+			request.Headers.Add(HttpRequestHeader.Authorization.ToString(), $"Bearer {accessToken}");
+			using (var response = request.GetResponse())
+			{
+				using (var responseStream = response.GetResponseStream())
+				{
+					graph = JsonHelper.Deserialize<GoogleUserData>(responseStream);
+				}
+			}
+
+			var userData = new Dictionary<string, string>();
+			userData.AddItemIfNotEmpty("id", graph.id);
+			userData.AddItemIfNotEmpty("name", graph.name);
+			userData.AddItemIfNotEmpty("given_name", graph.given_name);
+			userData.AddItemIfNotEmpty("email", graph.email);
+			return userData;
+		}
+
+		protected override string QueryAccessToken(Uri returnUrl, string authorizationCode)
+		{
+			var entity =
+				MessagingUtilities.CreateQueryString(
+					new Dictionary<string, string>
+					{
+						{"client_id", _clientId},
+						{"redirect_uri", returnUrl.AbsoluteUri},
+						{"client_secret", _clientSecret},
+						{"code", authorizationCode},
+						{"grant_type", "authorization_code"}
+					});
+
+			var tokenRequest = WebRequest.Create(_tokenEndpoint);
+			tokenRequest.ContentType = "application/x-www-form-urlencoded";
+			tokenRequest.ContentLength = entity.Length;
+			tokenRequest.Method = "POST";
+
+			using (var requestStream = tokenRequest.GetRequestStream())
+			{
+				var writer = new StreamWriter(requestStream);
+				writer.Write(entity);
+				writer.Flush();
+			}
+
+			var tokenResponse = (HttpWebResponse)tokenRequest.GetResponse();
+			if (tokenResponse.StatusCode == HttpStatusCode.OK)
+			{
+				using (var responseStream = tokenResponse.GetResponseStream())
+				{
+					var tokenData = JsonHelper.Deserialize<OAuth2AccessTokenData>(responseStream);
+					if (tokenData != null)
+					{
+						return tokenData.AccessToken;
+					}
+				}
+			}
+			return null;
+		}
+	}
+
+	public class GoogleUserData
+	{
+		public string id { get; set; }
+		public string name { get; set; }
+		public string given_name { get; set; }
+		public string email { get; set; }
+	}
+}
---
+++ src/AuthBridge/Protocols/OAuth/GoogleHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Claims;
+using System.Web;
+using AuthBridge.Clients;
+using AuthBridge.Model;
+using DotNetOpenAuth.AspNet;
+
+namespace AuthBridge.Protocols.OAuth
+{
+	public class GoogleHandler : ProtocolHandlerBase
+	{
+		private readonly ClaimProvider _issuer;
+		private readonly string _clientSecret;
+		private readonly string _clientId;
+
+		public GoogleHandler(ClaimProvider issuer) : base(issuer)
+		{
+			if (issuer == null)
+				throw new ArgumentNullException(nameof(issuer));
+			_issuer = issuer;
+			_clientId = _issuer.Parameters["clientId"];
+			_clientSecret = _issuer.Parameters["clientSecret"];
+		}
+
+		public override void ProcessSignInRequest(Scope scope, HttpContextBase httpContext)
+		{
+			var client = new GoogleOAuthClient(_clientId, _clientSecret);
+			client.RequestAuthentication(httpContext, MultiProtocolIssuer.ReplyUrl);
+		}
+
+		public override ClaimsIdentity ProcessSignInResponse(string realm, string originalUrl, HttpContextBase httpContext)
+		{
+			var client = new GoogleOAuthClient(_clientId, _clientSecret);
+			AuthenticationResult result;
+			try
+			{
+				result = client.VerifyAuthentication(httpContext, this.MultiProtocolIssuer.ReplyUrl);
+			}
+			catch (WebException wex)
+			{
+				throw new InvalidOperationException(new StreamReader(wex.Response.GetResponseStream()).ReadToEnd(), wex);
+			}
+
+			var claims = new List<Claim>
+			{
+				new Claim(System.IdentityModel.Claims.ClaimTypes.NameIdentifier, result.ExtraData["email"])
+			};
+
+			return new ClaimsIdentity(claims, "Google");
+		}
+	}
+}
