aff2960ec867de677e712005f6af6cc24b18bd1b
Kunning Mao <kunning.mao@teleopti.com>
2017-06-07 08:56:30 +0200

    minor refactor

--- src/AuthBridge.Web/Controllers/FederationMetadataController.cs
+++ src/AuthBridge.Web/Controllers/FederationMetadataController.cs
@@ -11,11 +11,11 @@ namespace AuthBridge.Web.Controllers
     using System.ServiceModel;
     using System.Web.Mvc;
 
-    using AuthBridge.Configuration;
+    using Configuration;
 
     public class FederationMetadataController : Controller
     {
-        private readonly IConfigurationRepository configuration;
+        private readonly IConfigurationRepository _configuration;
 
         public FederationMetadataController()
             : this(DefaultConfigurationRepository.Instance)
@@ -24,7 +24,7 @@ namespace AuthBridge.Web.Controllers
 
         public FederationMetadataController(IConfigurationRepository configuration)
         {
-            this.configuration = configuration;
+            _configuration = configuration;
         }
 
         [AcceptVerbs(HttpVerbs.Get)]
@@ -33,7 +33,7 @@ namespace AuthBridge.Web.Controllers
             var appRoot = HttpContext.GetRealAppRoot();
 			var signInUrl = new Uri(appRoot, Url.RouteUrl("Process Request"));
 
-            var serviceProperties = configuration.MultiProtocolIssuer;
+            var serviceProperties = _configuration.MultiProtocolIssuer;
 
             return File(GetFederationMetadata(signInUrl, serviceProperties.Identifier, serviceProperties.SigningCertificate), "text/xml");
         }
@@ -47,39 +47,45 @@ namespace AuthBridge.Web.Controllers
             var passiveEndpoint = new EndpointReference(passiveSignInUrl.AbsoluteUri);
 
             // Create metadata document for relying party
-            EntityDescriptor entity = new EntityDescriptor(new EntityId(realm.Uri.AbsoluteUri));
-            SecurityTokenServiceDescriptor sts = new SecurityTokenServiceDescriptor();
-            entity.RoleDescriptors.Add(sts);
+            var entity = new EntityDescriptor(new EntityId(realm.Uri.AbsoluteUri));
+            var sts = CreateSecurityTokenServiceDescriptor(credentials, passiveEndpoint);
 
-            // Add STS's signing key
-            KeyDescriptor signingKey = new KeyDescriptor(credentials.SigningKeyIdentifier);
-            signingKey.Use = KeyType.Signing;
-            sts.Keys.Add(signingKey);
-
-            // Add offered claim types
-            sts.ClaimTypesOffered.Add(new DisplayClaim(ClaimTypes.AuthenticationMethod));
-            sts.ClaimTypesOffered.Add(new DisplayClaim(ClaimTypes.AuthenticationInstant));
-            sts.ClaimTypesOffered.Add(new DisplayClaim(ClaimTypes.Name));
-
-            // Add passive federation endpoint
-            sts.PassiveRequestorEndpoints.Add(passiveEndpoint);
-
-            // Add supported protocols
-            sts.ProtocolsSupported.Add(new Uri(WSFederationConstants.Namespace));
-
-            // Add passive STS endpoint
-            sts.SecurityTokenServiceEndpoints.Add(passiveEndpoint);
-
-            // Set credentials with which to sign the metadata
-            entity.SigningCredentials = credentials;
+	        entity.RoleDescriptors.Add(sts);
+			// Set credentials with which to sign the metadata
+			entity.SigningCredentials = credentials;
 
             // Serialize the metadata and convert it to an XElement
-            MetadataSerializer serializer = new MetadataSerializer();
-            MemoryStream stream = new MemoryStream();
+            var serializer = new MetadataSerializer();
+            var stream = new MemoryStream();
             serializer.WriteMetadata(stream, entity);
             stream.Flush();
 
             return stream.ToArray();
         }
+
+	    private static SecurityTokenServiceDescriptor CreateSecurityTokenServiceDescriptor(X509SigningCredentials credentials,
+		    EndpointReference passiveEndpoint)
+	    {
+		    var sts = new SecurityTokenServiceDescriptor();
+
+		    // Add STS's signing key
+		    var signingKey = new KeyDescriptor(credentials.SigningKeyIdentifier) {Use = KeyType.Signing};
+		    sts.Keys.Add(signingKey);
+
+		    // Add offered claim types
+		    sts.ClaimTypesOffered.Add(new DisplayClaim(ClaimTypes.AuthenticationMethod));
+		    sts.ClaimTypesOffered.Add(new DisplayClaim(ClaimTypes.AuthenticationInstant));
+		    sts.ClaimTypesOffered.Add(new DisplayClaim(ClaimTypes.Name));
+
+		    // Add passive federation endpoint
+		    sts.PassiveRequestorEndpoints.Add(passiveEndpoint);
+
+		    // Add supported protocols
+		    sts.ProtocolsSupported.Add(new Uri(WSFederationConstants.Namespace));
+
+		    // Add passive STS endpoint
+		    sts.SecurityTokenServiceEndpoints.Add(passiveEndpoint);
+		    return sts;
+	    }
     }
 }
